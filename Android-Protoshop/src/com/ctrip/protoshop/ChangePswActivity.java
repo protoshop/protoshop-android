6972e389d2eb47d0aa6787352e818776add393c1
lxk李欣科 <xkli@ctrip.com>
2014-07-21 15:18:30 +0800

    重构修改密码

--- Android-Protoshop/src/com/ctrip/protoshop/ChangePswActivity.java
+++ Android-Protoshop/src/com/ctrip/protoshop/ChangePswActivity.java
@@ -7,7 +7,6 @@ import org.json.JSONObject;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.ActionBar;
 import android.text.TextUtils;
@@ -20,15 +19,15 @@ import android.widget.Toast;
 import com.android.volley.VolleyError;
 import com.ctrip.protoshop.constans.Constans;
 import com.ctrip.protoshop.constans.Function;
-import com.ctrip.protoshop.http.OnHttpListener;
 import com.ctrip.protoshop.util.MD5Util;
 import com.ctrip.protoshop.util.ProtoshopLog;
 import com.ctrip.protoshop.util.Util;
+import com.ctrip.protoshop.widget.HttpAsyncLayout;
+import com.ctrip.protoshop.widget.HttpAsyncLayout.OnHttpAsyncListner;
 
 public class ChangePswActivity extends BaseActivity {
 
-	private View mProgressView;
-
+	private HttpAsyncLayout mAsyncLayout;
 	private EditText mCurView;
 	private EditText mNewView;
 	private EditText mRepeatView;
@@ -53,12 +52,70 @@ public class ChangePswActivity extends BaseActivity {
 		actionBar.setDisplayHomeAsUpEnabled(true);
 		actionBar.setDisplayShowHomeEnabled(false);
 
+		mAsyncLayout = (HttpAsyncLayout) findViewById(R.id.http_async_layout);
+
 		mCurView = (EditText) findViewById(R.id.current_psw_view);
 		mNewView = (EditText) findViewById(R.id.new_psw_view);
 		mRepeatView = (EditText) findViewById(R.id.retype_psw_view);
 
-		mProgressView = findViewById(R.id.change_psw_loading_view);
+		addOnListener();
+	}
 
+	private void addOnListener() {
+		mAsyncLayout.setOnHttpAsyncListner(new OnHttpAsyncListner() {
+
+			@Override
+			public void onSuccessListener(String response) {
+				ProtoshopLog.e(response);
+
+				String resultStr = "修改成功";
+				try {
+					JSONObject resultObject = new JSONObject(response);
+					String status;
+					if (resultObject.has("status")) {
+						status = resultObject.getString("status");
+						if ("1".equals(status) && resultObject.has("code")) {
+
+							String code = resultObject.getString("code");
+							if ("3002".equals(code)) {
+								resultStr = "请重新登陆!";
+								LocalBroadcastManager.getInstance(ChangePswActivity.this).sendBroadcast(new Intent(Constans.LOGOUT));
+								startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+								finish();
+							} else if ("3005".equals(code)) {
+								resultStr = "服务器错误，请稍后再试!";
+							} else if ("3004".equals(code)) {
+								resultStr = "旧密码错误!";
+							} else {
+								resultStr = "未知错误,请联系开发者!";
+							}
+
+						} else if ("0".equals(status)) {
+							resultStr = "修改密码成功!";
+							finish();
+						}
+					}
+				} catch (JSONException e) {
+					e.printStackTrace();
+					resultStr = "解析出错!";
+				}
+
+				Toast.makeText(getApplicationContext(), resultStr, Toast.LENGTH_SHORT).show();
+
+			}
+
+			@Override
+			public void onRefreshListener() {
+			}
+
+			@Override
+			public void onErrorListener(VolleyError error) {
+
+				Toast.makeText(getApplicationContext(), "网络错误，请稍后再试!", Toast.LENGTH_SHORT).show();
+				ProtoshopLog.e("error", error.toString());
+
+			}
+		});
 	}
 
 	@Override
@@ -104,7 +161,6 @@ public class ChangePswActivity extends BaseActivity {
 			return;
 		}
 
-		mProgressView.setVisibility(View.VISIBLE);
 		Util.hideSoftkeyboard(getApplicationContext(), view);
 
 		HashMap<String, String> postParams = new HashMap<String, String>();
@@ -112,62 +168,7 @@ public class ChangePswActivity extends BaseActivity {
 		postParams.put("passwd", MD5Util.getMd5(mNewPswStr));
 		postParams.put("oldpwd", MD5Util.getMd5(mCurPswStr));
 
-		sendPostRequest(Function.CHANGE_PSW, postParams, new OnHttpListener() {
-
-			@Override
-			public void onErrorResponse(VolleyError error) {
-
-				Toast.makeText(getApplicationContext(), "网络错误，请稍后再试!", Toast.LENGTH_SHORT).show();
-				mProgressView.setVisibility(View.GONE);
-				ProtoshopLog.e("error", error.toString());
-
-			}
-
-			@Override
-			public void onResponse(String response) {
-				ProtoshopLog.e(response);
-
-				mProgressView.setVisibility(View.GONE);
-				String resultStr = "修改成功";
-				try {
-					JSONObject resultObject = new JSONObject(response);
-					String status;
-					if (resultObject.has("status")) {
-						status = resultObject.getString("status");
-						if ("1".equals(status) && resultObject.has("code")) {
-
-							String code = resultObject.getString("code");
-							if ("3002".equals(code)) {
-								resultStr = "请重新登陆!";
-								LocalBroadcastManager.getInstance(ChangePswActivity.this).sendBroadcast(new Intent(Constans.LOGOUT));
-								startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-								finish();
-							} else if ("3005".equals(code)) {
-								resultStr = "服务器错误，请稍后再试!";
-							} else if ("3004".equals(code)) {
-								resultStr = "旧密码错误!";
-							} else {
-								resultStr = "未知错误,请联系开发者!";
-							}
-
-						} else if ("0".equals(status)) {
-							resultStr = "修改密码成功!";
-							finish();
-						}
-					}
-				} catch (JSONException e) {
-					e.printStackTrace();
-					resultStr = "解析出错!";
-				}
-
-				Toast.makeText(getApplicationContext(), resultStr, Toast.LENGTH_SHORT).show();
-
-			}
-
-			@Override
-			public void onHttpStart() {
-			}
-		});
+		sendPostRequest(Function.CHANGE_PSW, postParams, mAsyncLayout);
 
 	}
 
