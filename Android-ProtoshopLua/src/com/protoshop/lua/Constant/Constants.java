14f890ee693f5c866326581ff4c577d6af0667e9
lxk李欣科 <xkli@ctrip.com>
2014-07-22 10:47:12 +0800

    添加back_home Action

--- Android-ProtoshopLua/src/com/protoshop/lua/Constant/Constants.java
+++ Android-ProtoshopLua/src/com/protoshop/lua/Constant/Constants.java
@@ -2,7 +2,8 @@ package com.protoshop.lua.Constant;
 
 public class Constants {
 
-    public static final String SCENE = "scene";
-    public static final String APPID = "appID";
+	public static final String SCENE = "scene";
+	public static final String APPID = "appID";
+	public static final String BACK_HOME = "back_home";
 
 }
