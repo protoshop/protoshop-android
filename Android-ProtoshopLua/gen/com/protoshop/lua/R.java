15c0d902ea7e615f0a57a9a2ae36c00eab28f29d
lxk李欣科 <xkli@ctrip.com>
2014-07-22 10:49:16 +0800

    回到首页

--- Android-ProtoshopLua/gen/com/protoshop/lua/R.java
+++ Android-ProtoshopLua/gen/com/protoshop/lua/R.java
@@ -9,26 +9,33 @@ package com.protoshop.lua;
 
 public final class R {
     public static final class anim {
-        public static int push_left_in=0x7f030000;
-        public static int push_left_out=0x7f030001;
-        public static int push_right_in=0x7f030002;
-        public static int push_right_out=0x7f030003;
+        public static int push_left_in=0x7f040000;
+        public static int push_left_out=0x7f040001;
+        public static int push_right_in=0x7f040002;
+        public static int push_right_out=0x7f040003;
     }
     public static final class attr {
     }
     public static final class drawable {
         public static int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static int cancel=0x7f060001;
+        public static int ok=0x7f060000;
+    }
+    public static final class layout {
+        public static int back_home_dialog=0x7f030000;
+    }
     public static final class raw {
-        public static int command=0x7f040000;
-        public static int ctrip=0x7f040001;
-        public static int demo=0x7f040002;
-        public static int lua_ctrip=0x7f040003;
-        public static int parase_lua=0x7f040004;
-        public static int test=0x7f040005;
+        public static int command=0x7f050000;
+        public static int ctrip=0x7f050001;
+        public static int demo=0x7f050002;
+        public static int lua_ctrip=0x7f050003;
+        public static int parase_lua=0x7f050004;
+        public static int test=0x7f050005;
     }
     public static final class string {
-        public static int app_name=0x7f050000;
+        public static int app_name=0x7f070000;
     }
     public static final class style {
         /** 
@@ -52,10 +59,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static int AppBaseTheme=0x7f060000;
+        public static int AppBaseTheme=0x7f080000;
+        public static int AppDialog=0x7f080002;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static int AppTheme=0x7f060001;
+        public static int AppTheme=0x7f080001;
     }
 }
