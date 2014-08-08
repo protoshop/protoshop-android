4aca2219b6043a561dc4b1a98a0d53e2aa653fa8
lxk李欣科 <xkli@ctrip.com>
2014-08-08 17:16:17 +0800

    动画

--- Android-Protoshop-Mini/src/com/ctrip/protoshop/mini/model/ActionModel.java
+++ Android-Protoshop-Mini/src/com/ctrip/protoshop/mini/model/ActionModel.java
@@ -2,5 +2,5 @@ package com.ctrip.protoshop.mini.model;
 
 public class ActionModel {
     public String target;
-    public String animationType;
+    public String animType;
 }
--- Android-Protoshop-Mini/src/com/ctrip/protoshop/mini/model/ProjectModel.java
+++ Android-Protoshop-Mini/src/com/ctrip/protoshop/mini/model/ProjectModel.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class ProjectModel {
 	public String appID;
 	public String appName;
+	public String appAnimType="0";
 	public String homeScene;
 	public String pageNum = "0";
 	public String appPlatform = "android";
